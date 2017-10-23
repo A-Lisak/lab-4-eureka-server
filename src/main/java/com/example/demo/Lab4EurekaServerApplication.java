950957fb1b3f37d97a95d1f67cf26fcb0dc5414a
A-Lisak <alex.lisak@esure.com>
2017-10-23 11:24:07 +0100

    ee

---
+++ src/main/java/com/example/demo/Lab4EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class Lab4EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4EurekaServerApplication.class, args);
+	}
+}
